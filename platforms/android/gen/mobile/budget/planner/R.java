5eb1173aaf17ba8147bddd406b49d022211c380c
kenvilar <kenvilar@gmail.com>
2014-03-09 00:25:11 +0800

    Fixed fb feed dialog; added fb javascript sdk method

--- platforms/android/gen/mobile/budget/planner/R.java
+++ platforms/android/gen/mobile/budget/planner/R.java
@@ -11,7 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int close=0x7f020000;
+        public static final int facebook_icon=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
         public static final int amount=0x7f060005;
